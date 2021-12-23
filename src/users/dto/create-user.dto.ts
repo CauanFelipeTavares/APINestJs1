64d6c1f0e29a80b2da761c9b2c12d5ac8261fd64
Cauan Felipe Tavares <tavarescauanf@gmail.com>
2021-12-23 01:53:07 -0300

    Decorators

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,24 @@
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength  } from "class-validator"
+
+
 export class CreateUserDto {
+
+    @MinLength(3, { message: "Digite um nome válido"})
+    @IsString()
     nome: String
+
+    @MinLength(3, { message: "Digite um sobrenome válido"})
+    @IsString()
+    sobrenome: String
+
+    @MinLength(8, { message: "A senha deve ter no mínimo 8 caracteres"})
+    @MaxLength(10, { message: "A senha deve ter no máximo 10 caracteres"})
     senha: String
+
+    @IsEmail({},{ message: "Digite um email válido"})
+    @IsNotEmpty()
     email: String
+
+
+    telefone: Number
 }
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { HttpCode } from '@nestjs/common';
 
 
 export type UserDocument = User & Document;
@@ -10,10 +11,16 @@ export class User {
   nome: string;
 
   @Prop({ required: true })
+  sobrenome: string;
+
+  @Prop({ required: true })
   senha: string;
 
   @Prop({ required: true })
   email: string;
+
+  @Prop({ required: false })
+  telefone: number;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);

72eb3e1822dbff909edd342f85a4f31784d38255
Cauan Felipe Tavares <tavarescauanf@gmail.com>
2022-01-10 16:07:59 -0300

    process.env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot('mongodb+srv://Calires:felipe23@cluster0.v9dm3.mongodb.net/test')],
+  imports: [UsersModule, MongooseModule.forRoot(`mongodb+srv://Calires:${process.env.DB_CLUSTER}@cluster0.v9dm3.mongodb.net/test`)],
   controllers: [],
   providers: [],
 })
